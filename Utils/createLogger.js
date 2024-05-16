30e2c52d0957dad8521aaa21065f328019e6928e
davque <davque@eltiempo.om>
2024-05-15 22:52:26 -0500

    Create tests to validate /user/login and /user endpoints

--- Utils/createLogger.js
+++ Utils/createLogger.js
@@ -5,7 +5,9 @@ export const logger = createLogger({
   level: 'debug',
   format: format.json(),
   transports: (NODE_ENV === 'test')
-    ? []
+    ? [
+        new transports.File({ filename: ERR_LOG_FILE, level: 'error' })
+      ]
     : [
         new transports.Console(),
         new transports.File({ filename: ERR_LOG_FILE, level: 'error' })
--- tests/users.test.js
+++ tests/users.test.js
@@ -3,11 +3,36 @@ import request from 'supertest'
 
 const api = request(app)
 
-describe('Users endpoints', () => {
+let jwt
+
+test('Jwt is returned in login endpoint', async () => {
+  const result = await api
+    .post('/user/login')
+    .send(
+      {
+        email: 'admin@admin.com',
+        user_password: 'AdminPasS'
+      }
+    )
+
+  // get jwt for testint the rest of jest test
+  jwt = result.body.jwt
+})
+
+describe('/user endpoint', () => {
   test('Users are returned as json', async () => {
     await api
       .get('/user')
+      .set('Authorization', jwt)
+      .expect(200)
       .expect('Content-Type', /application\/json/)
+  })
+
+  test('validate first user created by default, user admin', async () => {
+    const response = await api.get('/user').set('Authorization', jwt)
+
+    expect(response.body[0].first_name).toBe('admin')
+    expect(response.body[0].last_name).toBe('admin')
   })
 })
 
